07ea097ae04e91589d934941352ec0e4d65c35e6
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-01 17:19:55 -0300

    req 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,11 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("trybe", 10) == "ebyrt"
+    assert encrypt_message("trybe", 3) == "yrt_eb"
+    assert encrypt_message("trybe", 2) == "eby_rt"
+
+    with pytest.raises(TypeError):
+        encrypt_message("trybe", "str")
