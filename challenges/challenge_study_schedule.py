6c2c1f4541450fa20ccca27e9dfb40c23b426495
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-01 15:28:25 -0300

    req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,9 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    contador = 0
+    for start, end in permanence_period:
+        try:
+            if start <= target_time <= end:
+                contador += 1
+        except TypeError:
+            return None
+    return contador
