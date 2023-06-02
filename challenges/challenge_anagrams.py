b3de80d59a757e8d8ce08a91cd6bba42c4d3cf24
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-01 23:25:37 -0300

    req 4 ajuste

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,28 +1,26 @@
-def merge_sort(string):
-    if len(string) <= 1:
-        return string
-    mid = len(string) // 2
-    left, right = merge_sort(string[:mid]), merge_sort(string[:mid])
-    return combine(left, right)
-
-
-def combine(left, right):
+def compare(left, right):
     if not right:
         return left
     if not left:
         return right
     if right[0] < left[0]:
-        return right[0] + combine(left, right[1:])
+        return right[0] + compare(left, right[1:])
     else:
-        return left[0] + combine(left[1:], right)
+        return left[0] + compare(left[1:], right)
 
 
 def is_anagram(first_string, second_string):
     if first_string == "" and second_string == "":
         return (first_string, second_string, False)
-    first_string = merge_sort(list(first_string.lower()))
-    second_string = merge_sort(list(second_string.lower()))
 
-    x = ''.join(first_string)
-    y = ''.join(second_string)
-    return (x, y, x == y)
+    x = merge_sort(first_string.lower())
+    z = merge_sort(second_string.lower())
+    return (x, z, x == z)
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left, right = merge_sort(array[:mid]), merge_sort(array[mid:])
+    return compare(left, right)
