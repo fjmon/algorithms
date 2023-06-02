25d7b0f66f38aa5c6496f95e36aa9c48df09b09b
Fernando Monteiro <ferjmonteiro@gmail.com>
2023-06-01 21:36:42 -0300

    req 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,28 @@
-def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
+def merge_sort(string):
+    if len(string) <= 1:
+        return string
+    mid = len(string) // 2
+    left, right = merge_sort(string[:mid]), merge_sort(string[:mid])
+    return combine(left, right)
 
-    raise NotImplementedError
+
+def combine(left, right):
+    if not right:
+        return left
+    if not left:
+        return right
+    if right[0] < left[0]:
+        return right[0] + combine(left, right[1:])
+    else:
+        return left[0] + combine(left[1:], right)
+
+
+def is_anagram(first_string, second_string):
+    if first_string == "" and second_string == "":
+        return (first_string, second_string, False)
+    first_string = merge_sort(list(first_string.lower()))
+    second_string = merge_sort(list(second_string.lower()))
+
+    x = ''.join(first_string)
+    y = ''.join(second_string)
+    return (x, y, x == y)
